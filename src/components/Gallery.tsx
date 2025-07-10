dafd5cdc8808b6e8a419f4abccedc500102103bc
Piotr Koćwin <wolfdaleton@gmail.com>
2025-07-10 19:15:17 +0200

    Refactor Gallery component to use ImageGallery and update styles; add new GalleryStyle.scss

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
+import './styles/GalleryStyle.scss';
 
 interface GalleryProps {
   images: string[];
 }
 
 const Gallery: React.FC<GalleryProps> = ({ images }) => {
+
+  const galleryImages = images.map((img) => ({
+    original: img,
+    thumbnail: img,
+  }));
+
   return (
     <div className="gallery">
-      {images.map((image, index) => (
-        <div className="gallery-item" key={index}>
-          <img src={image} alt={`Gallery item ${index + 1}`} />
-        </div>
-      ))}
+      <ImageGallery items={galleryImages} showPlayButton={false} />
     </div>
   );
 };
